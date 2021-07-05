9330ae1b3e587176f246fd6ff7eb9e0f428257c3
Judacar <Judacar2@gmail.com>
2021-07-05 17:07:53 -0500

    Se cambiaron estilos y se agrego la funcion seleccionar carta

--- js/ui.js
+++ js/ui.js
@@ -15,8 +15,25 @@ function createCard(title, id) {
   const cardDiv = document.createElement('DIV');
   cardDiv.classList.add('card');
 
+  cardDiv.onclick = selectCard;
+
   cardDiv.appendChild(cardTitle);
 
   const cardsContainer = document.querySelector('#cards-container');
   cardsContainer.appendChild(cardDiv);
 }
+
+function selectCard (e){
+  let element;
+  if (e.target.tagName === 'P'){
+    element = e.target.parentElement;
+  } else {
+    element = e.target;
+  };
+  
+  if (element.classList.contains('card-selected')){
+    element.classList.remove('card-selected');
+  } else {
+    element.classList.add ('card-selected');
+  }
+};
