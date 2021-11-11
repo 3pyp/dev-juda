0f4883e0448a70472734a5d88def56baf5104a2b
Cristian Gutierrez <cristiandisenadorvisual@gmail.com>
2021-11-10 21:26:32 -0500

    Peticiones en secuencia agregadas

--- javascript/Peticiones/index.js
+++ javascript/Peticiones/index.js
@@ -3,19 +3,42 @@
 //   .then(data => console.log(data));
 const container = document.querySelector('#container');
 // let champions = []
+const LOL_URL = 'http://ddragon.leagueoflegends.com/cdn/11.2.1/data/es_MX/champion.json';
+const POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/';
 
-fetch('http://ddragon.leagueoflegends.com/cdn/11.2.1/data/es_MX/champion.json')
-  .then((response) => response.json())
-  .then((data) => showNames(data.data))
-  .catch((error)=>{
-    console.log(error)
-  })
+// function getPokemon(pokemon) {
+//   fetch(POKE_API_URL + pokemon)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       showNames(data)
+//       return fetch(data.abilities[0].ability.url)
+//     })
+//     .then((response) => response.json())
+//     .then((data)=> showNames({data}))
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }
+
+function getLolChampions() {
+  fetch(LOL_URL)
+    .then((response) => response.json())
+    .then((data) => showNames(data.data))
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 function showNames(obj) {
   for (let attr in obj) {
     const child = document.createElement('p');
+    const image = document.createElement('img');
+    image.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${obj[attr].id}_0.jpg`
     child.textContent = obj[attr].name;
 
+    container.appendChild(image);
     container.appendChild(child);
   }
 }
+
+getLolChampions()
